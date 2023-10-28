681649725487b43a7763526758e673bce63d9e32
savaandrei2003 <savaandreidragos2@gmail.com>
2023-10-28 22:37:11 +0300

    home page done

--- frontend/src/common/components/Page/Page.tsx
+++ frontend/src/common/components/Page/Page.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import './page.css';
 import img1 from '../../assets/header-mobile-green.png';
+import { faHamburger, faHome, faMap, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 interface IPageProps {
   children: JSX.Element;
 }
 
 export default function Page({ children }: IPageProps) {
+
+  type Icon = {
+    icon: any; // Aici ar trebui să fie tipul corect pentru iconuri
+    label: string;
+  };
+  
+  const icons: Icon[] = [
+    { icon: faHome, label: 'Acasa' },
+    { icon: faMap, label: 'Harta' },
+    { icon: faSearch, label: 'Cauta' },
+    { icon: faHamburger, label: 'Stats' }
+    
+  ];
+
+
   return (
     <div className='page flex flex-col '>
       <div className='flex items-center justify-start text-center pt-8'>
@@ -18,7 +35,17 @@ export default function Page({ children }: IPageProps) {
 
       <img src={img1} className='mt-[-25px]' alt='mama' /> 
       
-      <div className="">{children}</div>
+      <div className="scroll">{children}</div>
+
+      <div className='flex flex-row w-full h-16 side justify-around items-center  ' >
+        {icons.map((icon: Icon) => (
+          <div className='flex flex-col items-center justify-center'>
+            <FontAwesomeIcon icon={icon.icon} className='text-white' />
+            <p className='text-white'>{icon.label}</p>
+          </div>
+        ))}
+        
+      </div>
     </div>
   );
 }
--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -4,8 +4,71 @@ import '../../index.css'
 import './home.css'
 import img2 from '../../common/assets/Pinpoint-white.png';
 import img3 from '../../common/assets/calendar.png';
+import Card from './components/Card';
+import { ICard } from '../../common/interfaces/card.interface';
+import { IHome } from '../../common/interfaces/home.interface';
+
 
 export default function Home() {
+
+    const homeInfo: IHome = {
+        user_name: "Utilizator",
+        location_home: "Mama ta",
+        index_home: 3,
+        cards: [
+            {
+                name: "Nume 1",
+                location: "Locație 1",
+                hour: "08:00",
+                temperature: 23,
+                index: 1,
+            },
+            {
+                name: "Nume 2",
+                location: "Locație 2",
+                hour: "09:00",
+                temperature: 24,
+                index: 2,
+            },
+            {
+                name: "Nume 3",
+                location: "Locație 3",
+                hour: "10:00",
+                temperature: 25,
+                index: 3,
+            },
+            {
+                name: "Nume 3",
+                location: "Locație 3",
+                hour: "10:00",
+                temperature: 25,
+                index: 4,
+            },
+            {
+                name: "Nume 3",
+                location: "Locație 3",
+                hour: "10:00",
+                temperature: 25,
+                index: 5,
+            }
+        ],
+    };
+
+    let colorClass = '';
+    let text='';
+
+  if (homeInfo.index_home === 4 || homeInfo.index_home  === 5) {
+    colorClass = 'bg-gradient-to-l from-green-500 to-green-8000';
+    text='EXCELENTA';
+  } else if (homeInfo.index_home  === 3) {
+    colorClass = 'bg-gradient-to-l from-yellow-500';
+    text='MEDIE';
+  } else if (homeInfo.index_home  === 1 || homeInfo.index_home === 2) {
+    colorClass = 'bg-gradient-to-l from-red-500';
+    text='SLABĂ';
+  }
+
+
     return (
     <Page>
         
@@ -15,7 +78,7 @@ export default function Home() {
                     Salut,&nbsp; 
                 </h1>
                 <h1 className="text-3xl text-white second-title tracking-normal">
-                    Eduard
+                    {homeInfo.user_name}
                 </h1>
             </div>
 
@@ -25,16 +88,16 @@ export default function Home() {
             </div>
             <div className='flex flex-row text-white items-center justify-center text-center'>
                 <img src={img2} className='pinpoint' alt="mama"></img> 
-                <p className='locatie tracking-wide' >&nbsp; Bucharest, Bd. Iuliu Maniu:</p>
+                <p className='locatie tracking-wide' >&nbsp;Locație: {homeInfo.location_home}</p>
 
             </div>
         </div>
 
-        <div className='flex result items-center justify-center text-center mt-11 main-gradient'>
-            EXCELENTA
+        <div className={`flex result items-center justify-center text-center mt-11 main-gradient ${colorClass}`}>
+            {text}
         </div>
 
-        <div className='flex flex-col mt-12'>
+        <div className='flex flex-col mt-12 mb-10'>
             <div className=' text-center justify-center calitate'>
                 <p className=''> Calitatea mediului astăzi</p>
             </div>
@@ -43,6 +106,25 @@ export default function Home() {
                 <p className='locatie tracking-wide' >&nbsp; 28.10.2023</p>
 
             </div>
+        </div>
+
+        <div className='flex flex-col w-5/6 mb-20  '>
+
+            {homeInfo.cards.map((card: ICard) => (
+              <Card
+                name={card.name}
+                location={card.location}
+                hour={card.hour}
+                temperature={card.temperature}
+                index={card.index}
+                
+              />
+            ))}
+
+
+
+
+
         </div>
         
         
