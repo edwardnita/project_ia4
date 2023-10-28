1c7333049431d719002d826f7ac8e372ec501cef
savaandrei2003 <savaandreidragos2@gmail.com>
2023-10-28 18:55:20 +0300

    nav + salut,eduard

--- frontend/src/common/components/Page/Page.tsx
+++ frontend/src/common/components/Page/Page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-// import Footer from './Footer';
 import './page.css';
+import img1 from '../../assets/header-mobile-green.png';
 
 interface IPageProps {
   children: JSX.Element;
@@ -8,10 +8,17 @@ interface IPageProps {
 
 export default function Page({ children }: IPageProps) {
   return (
-    <div className='page'>
-     
+    <div className='page flex flex-col '>
+      <div className='flex items-center justify-start text-center pt-8'>
+        <div className='flex flex-row'>
+          <h1 className='pl-10 green'>Green</h1>
+          <h1 className='wave '>Wave</h1>
+        </div>
+      </div>
+
+      <img src={img1} className='mt-[-25px]' alt='mama' /> 
+      
       <div className="">{children}</div>
-     
     </div>
   );
 }
--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,17 +1,42 @@
 import React from 'react';
 import Page from '../../common/components/Page/Page'
 import '../../index.css'
+import './home.css'
+import img2 from '../../common/assets/Pinpoint-white.png';
 
 export default function Home() {
     return (
     <Page>
         
-        
-        <div className="container"> 
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
+       <div className='flex flex-col mt-7 items-center justify-center'>
+            <div className="flex flex-row px-5 mb-10 items-center justify-center text-center "> 
+                <h1 className="text-3xl text-white main-title tracking-normal ">
+                    Salut,&nbsp; 
+                </h1>
+                <h1 className="text-3xl text-white second-title tracking-normal">
+                    Eduard
+                </h1>
+            </div>
+
+        <div className='flex flex-col'>
+            <div className=' text-center justify-center calitate'>
+                <p className=''> Calitatea mediului în</p>
+            </div>
+            <div className='flex flex-row text-white items-center justify-center text-center'>
+                <img src={img2} className='pinpoint' alt="mama"></img> 
+                <p className='locatie tracking-wide' >&nbsp; Bucharest, Bd. Iuliu Maniu:</p>
+
+            </div>
+            
+
+
         </div>
+
+        
+        
+        
+        </div> 
+        
    </Page>
     );
 
