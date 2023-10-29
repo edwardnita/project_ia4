5afafaf4a5bc88cf8fe855486fef42f1939d45d5
savaandrei2003 <savaandreidragos2@gmail.com>
2023-10-29 03:53:31 +0200

    legare front-back

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Page from '../../common/components/Page/Page'
 import '../../index.css'
 import './home.css'
@@ -9,52 +9,80 @@ import { ICard } from '../../common/interfaces/card.interface';
 import { IHome } from '../../common/interfaces/home.interface';
 import Salut from './components/Salut';
 import Status from './components/Status';
+import { getHome } from '../../common/services/api/home';
 
 
 export default function Home() {
 
-    const homeInfo: IHome = {
-        user_name: "Andrei",
-        location_home: "Bd. Iuliu Maniu, Bucharest",
-        index_home: 1,
-        cards: [
-            {
-                name: "Nume 1",
-                location: "Locație 1",
-                hour: "08:00",
-                temperature: 23,
-                index: 1,
-            },
-            {
-                name: "Nume 2",
-                location: "Locație 2",
-                hour: "09:00",
-                temperature: 24,
-                index: 2,
-            },
-            {
-                name: "Nume 3",
-                location: "Locație 3",
-                hour: "10:00",
-                temperature: 25,
-                index: 3,
-            },
-            {
-                name: "Nume 3",
-                location: "Locație 3",
-                hour: "09:00",
-                temperature: 25,
-                index: 4,
-            },
-            {
-                name: "Nume 3",
-                location: "Locație 3",
-                hour: "18:00",
-                temperature: 25,
-                index: 5,
-            }
-        ],
-    };
+    // const homeInfo: IHome = {
+    //     user_name: "Andrei",
+    //     location_home: "Bd. Iuliu Maniu, Bucharest",
+    //     index_home: 1,
+    //     cards: [
+    //         {
+    //             name: "Nume 1",
+    //             location: "Locație 1",
+    //             hour: "08:00",
+    //             temperature: 23,
+    //             index: 1,
+    //         },
+    //         {
+    //             name: "Nume 2",
+    //             location: "Locație 2",
+    //             hour: "09:00",
+    //             temperature: 24,
+    //             index: 2,
+    //         },
+    //         {
+    //             name: "Nume 3",
+    //             location: "Locație 3",
+    //             hour: "10:00",
+    //             temperature: 25,
+    //             index: 3,
+    //         },
+    //         {
+    //             name: "Nume 3",
+    //             location: "Locație 3",
+    //             hour: "09:00",
+    //             temperature: 25,
+    //             index: 4,
+    //         },
+    //         {
+    //             name: "Nume 3",
+    //             location: "Locație 3",
+    //             hour: "18:00",
+    //             temperature: 25,
+    //             index: 5,
+    //         }
+    //     ],
+    // };
+
+    // const [cards, setCards] = useState<ICard[]>([]);
+    const [homeInfo, setHomeInfo] = useState<IHome>({
+        user_name: '',
+        location_home: '',
+        index_home: 0,
+        cards: [],
+    });
+
+    const [isLoading, setIsLoading] = useState(false);
+    const [isError, setIsError] = useState(false);
+  
+    useEffect(() => {
+      setIsLoading(true);
+  
+      getHome()
+        .then(response => {
+          setHomeInfo(response.data);
+  
+          setIsError(false);
+          setIsLoading(false);
+        })
+        .catch(() => {
+          setIsError(true);
+          setIsLoading(false);
+        });
+    }, []);
 
     let colorClass = '';
     let text='';
@@ -96,16 +124,29 @@ export default function Home() {
             
         <div className='flex flex-col w-5/6 sm:w-1/2 mb-20 sm:mb-28 lg:w-1/3 '>
 
-            {homeInfo.cards.map((card: ICard) => (
-              <Card
-                name={card.name}
-                location={card.location}
-                hour={card.hour}
-                temperature={card.temperature}
-                index={card.index}
-                
-              />
-            ))}
+            {!isLoading ? (
+            !isError && homeInfo.cards.length !== 0 ? (
+              <div >
+                {homeInfo.cards.map((home: ICard) => (
+                  <Card
+                    key={home.index}
+                    name={home.name}
+                    location={home.location}
+                    hour={home.hour}
+                    temperature={home.temperature}
+                    index={home.index}
+                    
+                  />
+                ))}
+              </div>
+                ) : (
+                  <p>Esti prost</p>
+                )
+              ) : (
+                <p>Bravo</p>
+            )}
+
+            
 
         </div>
         
